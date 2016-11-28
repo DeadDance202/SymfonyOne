960f1ef142bd33dcecc3b6c24e28b73c3bcff4f5
DeadDance202 <DeadDance202@mail.ru>
2016-11-28 09:46:36 +0200

    userpag2

--- src/BlogBundle/Controller/KnpController.php
+++ src/BlogBundle/Controller/KnpController.php
@@ -3,7 +3,7 @@ namespace BlogBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class KnpController extends Controller
 {
@@ -11,14 +11,22 @@ class KnpController extends Controller
      * @Route(path="/{id}", name="category", requirements={"id": "\d+"})
      * @Route("/blog/{page}", name="blog_list", requirements={"page": "\d+"})
      */
-public function indexAction($id)
+    public $articlesPerPage = 8;
+    public function indexAction(Request $request,$id)
     {
 
-    $repository=$this->getDoctrine()->getRepository('BlogBundle:Product');
-    $products=$repository->findBy(array('category'=>$id));
+        $repository=$this->getDoctrine()->getRepository('BlogBundle:Product');
+        $products=$repository->findBy(array('category'=>$id));
         $em = $this->getDoctrine()->getManager();
         $name = $em->getRepository('BlogBundle:Category')->find($id);
-    return $this->render('BlogBundle:Knp:index.html.twig',array('products'=>$products,'name'=>$name));
+
+        $articles = $repository->findAll();
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $articles,
+            $request->query->getInt('page', 1),
+            $this->articlesPerPage);
+        return $this->render('BlogBundle:Knp:index.html.twig',array('products'=>$products,'name'=>$name,'pagination' => $pagination));
 
     }
 }
